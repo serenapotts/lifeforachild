944eaa70703599dbc4e81203ec27b49bcb4ccc80
serenapotts <serenapotts@users.noreply.github.com>
2009-11-20 12:34:47 +0000

    add all child search fields to query with stubs to be completed

--- src/main/java/org/lifeforachild/web/query/ChildQuery.java
+++ src/main/java/org/lifeforachild/web/query/ChildQuery.java
@@ -11,9 +11,13 @@ import net.java.dev.querybuilder.JPAQueryBuilder;
 
 import org.lifeforachild.Util.StringUtil;
 import org.lifeforachild.domain.Child;
+import org.lifeforachild.domain.Country;
 import org.lifeforachild.domain.DateRange;
+import org.lifeforachild.domain.DiabetesCentre;
 import org.lifeforachild.domain.Report;
 import org.lifeforachild.domain.Search;
+import org.lifeforachild.domain.ShowOptionType;
+import org.lifeforachild.domain.StatusType;
 import org.lifeforachild.domain.TimePeriodUnit;
 
 public class ChildQuery extends BaseQuery {
@@ -38,36 +42,105 @@ public class ChildQuery extends BaseQuery {
 				ex.printStackTrace();
 			}	
 		}
+		// TODO convert these to objects in the Search class and in UI
 		String centre = search.getCentre();
 		String country = search.getCountry(); 
-		return getQuery(entityManager, id, timePeriod, timePeriodUnit, fromDate, toDate);
+		return getQuery(entityManager, id, timePeriod, timePeriodUnit, fromDate, toDate, null, null);
 	}
 	
 	public static List getQuery(EntityManager entityManager, Report report)
 	{
 		return getQuery(entityManager, null, report.getTimePeriod(), report.getTimeperiodunit(), 
-				report.getFromDate(), report.getToDate());
+				report.getFromDate(), report.getToDate(), report.getCentre(), report.getCountry(),
+				report.getStatustype(), report.getShowoptiontype(), report.getRecordNumber(), 
+				report.getAge(), report.getWeight(), report.getHeight(), report.getOrderBy(), report.getThenOrderBy());
 	}
 	
 	private static List getQuery(EntityManager entityManager, String id, String timePeriod, 
-			TimePeriodUnit timePeriodUnit, Date from, Date to)
+			TimePeriodUnit timePeriodUnit, Date from, Date to, DiabetesCentre diabetesCentre, Country country)
+	{
+		return getQuery(entityManager, id, timePeriod, timePeriodUnit, from, to, diabetesCentre, country, null,
+				null, null, null, null, null, null, null);
+	}
+	
+	private static List getQuery(EntityManager entityManager, String id, String timePeriod, 
+			TimePeriodUnit timePeriodUnit, Date from, Date to, DiabetesCentre diabetesCentre, Country country,
+			StatusType statusType, ShowOptionType showOptionType, String recordNumber, String age, String weight, 
+			String height, String orderBy, String thenOrderBy)
 	{
 		JPAQueryBuilder builder = new JPAQueryBuilder();
 		builder.select("o");
 		builder.from("Child o");
+		
 		searchByID(builder, id);
 		searchByTimePeriod(builder, timePeriod, timePeriodUnit);
 		searchByDateRange(builder, from, to);
-		/*if (!StringUtil.isEmpty(centre))
-		appendOperator(buffer, "centre", centre, EQUALS_OPERATOR);
-	if (!StringUtil.isEmpty(country))
-		appendOperator(buffer, "country", country, EQUALS_OPERATOR);*/
+		searchByDiabetesCentre(builder, diabetesCentre);
+		searchByCountry(builder, country);
+		searchByStatusType(builder, statusType);
+		searchByShowOptionType(builder, showOptionType);
+		searchByRecordNumber(builder, recordNumber);
+		searchByAge(builder, age);
+		searchByWeight(builder, weight);
+		searchByHeight(builder, height);
+		orderBy(builder, orderBy, thenOrderBy);
 
 		Query query = builder.build(entityManager);
 		return query.getResultList();
 
 	}
 	
+	private static void searchByShowOptionType(JPAQueryBuilder builder,
+			ShowOptionType showOptionType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void orderBy(JPAQueryBuilder builder, String orderBy, String thenOrderBy) {
+		if (!StringUtil.isEmpty(orderBy))
+			builder.orderBy(orderBy);
+		if (!StringUtil.isEmpty(thenOrderBy))
+			builder.orderBy(thenOrderBy);			
+	}
+
+	private static void searchByHeight(JPAQueryBuilder builder, String height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchByWeight(JPAQueryBuilder builder, String weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchByAge(JPAQueryBuilder builder, String age) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchByRecordNumber(JPAQueryBuilder builder,
+			String recordNumber) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchByStatusType(JPAQueryBuilder builder,
+			StatusType statusType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchByCountry(JPAQueryBuilder builder, Country country) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void searchByDiabetesCentre(JPAQueryBuilder builder,
+			DiabetesCentre diabetesCentre) {
+		//builder.andIfNotNull(condition, object)
+		
+	}
+
 	private static void searchByID(JPAQueryBuilder builder, String id)
 	{
 		if (!StringUtil.isEmpty(id))
