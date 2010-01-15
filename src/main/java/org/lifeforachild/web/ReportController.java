b02ab0b1b433b54978d754de8f8a73ba48364374
serenapotts <serenapotts@users.noreply.github.com>
2010-01-15 09:30:43 +0000

    turn off maintin view so don't overwrite manual changes I made

--- src/main/java/org/lifeforachild/web/ReportController.java
+++ src/main/java/org/lifeforachild/web/ReportController.java
@@ -5,7 +5,7 @@ import org.lifeforachild.domain.Report;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
-@RooWebScaffold(path = "report", automaticallyMaintainView = true, formBackingObject = Report.class)
+@RooWebScaffold(path = "report", automaticallyMaintainView = false, formBackingObject = Report.class)
 @RequestMapping("/report/**")
 @Controller
 public class ReportController {
