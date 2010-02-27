09144c7c846106ee6b4b75af4dadcfa9d6f067a6
serenapotts <serenapotts@users.noreply.github.com>
2010-02-27 05:36:18 +0000

    tidy imports

--- src/main/java/org/lifeforachild/domain/UserGroup.java
+++ src/main/java/org/lifeforachild/domain/UserGroup.java
@@ -1,19 +1,16 @@
 package org.lifeforachild.domain;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
-import org.hibernate.annotations.CollectionOfElements;
-import org.hibernate.annotations.IndexColumn;
+
 import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.tostring.RooToString;
-import java.util.Set;
-import org.lifeforachild.domain.Permissions;
-import java.util.HashSet;
-import javax.persistence.ManyToMany;
-import javax.persistence.CascadeType;
 
 @Entity
 @RooJavaBean
