9edad6aa93d85a2adff014282a5b2c76ae039c31
serenapotts <serenapotts@users.noreply.github.com>
2010-09-23 09:57:49 +0000

    make password stronger

--- src/main/java/org/lifeforachild/domain/User.java
+++ src/main/java/org/lifeforachild/domain/User.java
@@ -3,6 +3,7 @@ package org.lifeforachild.domain;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
@@ -29,6 +30,9 @@ public class User {
     String username;
     
 	@NotNull
+	// has to contain 1 uppercase, 1 lowercase, 1 number of special char and be 8 characters in length
+	@Pattern(regexp="(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$", 
+			message ="Password is not strong enough. Must contain an uppercase, lowercase and number or special character and be 8 characters in length.")	
     String password;
     
 	@NotNull
