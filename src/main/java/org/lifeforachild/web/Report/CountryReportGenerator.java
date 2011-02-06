ae14a2e0b4069b53476df1e3a09b42f8dd6f7a82
serenapotts <serenapotts@users.noreply.github.com>
2011-02-06 08:56:35 +0000

    show list of sub items in excel report to make more useful

--- src/main/java/org/lifeforachild/web/Report/CountryReportGenerator.java
+++ src/main/java/org/lifeforachild/web/Report/CountryReportGenerator.java
@@ -1,10 +1,15 @@
 package org.lifeforachild.web.Report;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.lifeforachild.domain.Country;
+import org.lifeforachild.domain.DiabetesCentre;
 import org.lifeforachild.domain.Report;
 
+import ar.com.fdvs.dj.domain.CustomExpression;
 import ar.com.fdvs.dj.domain.builders.ColumnBuilderException;
 import ar.com.fdvs.dj.domain.builders.DynamicReportBuilder;
 
@@ -25,6 +30,28 @@ public class CountryReportGenerator extends ReportGenerator {
 	void addColumns(DynamicReportBuilder drb, Object[] fields) throws ColumnBuilderException {
 		// these columns are displayed on every report
 		addColumn(drb, "name", "Name", String.class, 85);
+		drb.addField("diabetesCentres", String.class.getName());        
+        addColumn(drb, "diabetesCentres", "Centres", DiabetesCentre.class, 200, null, 
+    		new CustomExpression() {
+	            public Object evaluate(Map fields, Map variables, Map parameters) {	   
+	            	Set centres = (Set)fields.get("diabetesCentres");
+	            	StringBuffer centresStr = new StringBuffer();
+	            	Iterator it = centres.iterator();
+	            	while (it.hasNext())
+	            	{
+	            		DiabetesCentre centre = (DiabetesCentre)it.next();
+	            		centresStr.append(centre.getName());
+	            		if (it.hasNext())
+	            			centresStr.append(", ");
+	            	}
+	            	return centresStr.toString();
+	            }
+	
+	            public String getClassName() {
+	                    return String.class.getName();
+	            }
+        	}
+        );
 	}
 
 	// none of these methods are used but need to be defined to extend the report generator class
--- src/main/java/org/lifeforachild/web/Report/UserGroupReportGenerator.java
+++ src/main/java/org/lifeforachild/web/Report/UserGroupReportGenerator.java
@@ -1,10 +1,16 @@
 package org.lifeforachild.web.Report;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.lifeforachild.domain.DiabetesCentre;
+import org.lifeforachild.domain.Permissions;
 import org.lifeforachild.domain.Report;
 import org.lifeforachild.domain.UserGroup;
 
+import ar.com.fdvs.dj.domain.CustomExpression;
 import ar.com.fdvs.dj.domain.builders.ColumnBuilderException;
 import ar.com.fdvs.dj.domain.builders.DynamicReportBuilder;
 
@@ -25,6 +31,28 @@ public class UserGroupReportGenerator extends ReportGenerator {
 	void addColumns(DynamicReportBuilder drb, Object[] fields) throws ColumnBuilderException {
 		// these columns are displayed on every report
 		addColumn(drb, "groupName", "Group Name", String.class, 85);
+		drb.addField("permissions", String.class.getName());        
+        addColumn(drb, "permissions", "Permissions", Permissions.class, 200, null, 
+    		new CustomExpression() {
+	            public Object evaluate(Map fields, Map variables, Map parameters) {	   
+	            	Set permissions = (Set)fields.get("permissions");
+	            	StringBuffer permissionsStr = new StringBuffer();
+	            	Iterator it = permissions.iterator();
+	            	while (it.hasNext())
+	            	{
+	            		Permissions permission = (Permissions)it.next();
+	            		permissionsStr.append(permission.getDescription());
+	            		if (it.hasNext())
+	            			permissionsStr.append(", ");
+	            	}
+	            	return permissionsStr.toString();
+	            }
+	
+	            public String getClassName() {
+	                    return String.class.getName();
+	            }
+        	}
+        );
 	}
 
 	// none of these methods are used but need to be defined to extend the report generator class
