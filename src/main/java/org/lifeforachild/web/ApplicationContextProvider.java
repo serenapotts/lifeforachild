ece34e6531ecdb963cc694a1e7d080d0480fd7c0
serenapotts <serenapotts@users.noreply.github.com>
2011-02-13 20:36:54 +0000

    Update enums so they support I18N

---
+++ src/main/java/org/lifeforachild/web/ApplicationContextProvider.java
@@ -0,0 +1,21 @@
+package org.lifeforachild.web;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+
+public class ApplicationContextProvider implements ApplicationContextAware { 
+
+ 
+
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException { 
+
+        // Wiring the ApplicationContext into a static method 
+
+        AppContext.setApplicationContext(ctx); 
+
+    } 
+
+}
