013d5970465a57ccda56f962219ffdd9e2f1f42d
serenapotts <serenapotts@users.noreply.github.com>
2011-02-13 20:36:29 +0000

    output tab delimted file as there are commas in the text

--- src/main/java/org/lifeforachild/Util/PropertiesToCSV.java
+++ src/main/java/org/lifeforachild/Util/PropertiesToCSV.java
@@ -12,13 +12,13 @@ public class PropertiesToCSV {
 
 	public static void main(String[] args)
 	{
-		Properties properties = new Properties(); 
+		Properties properties = new Properties();
 		Properties properties_es = new Properties(); 
 		try { 
 			properties.load(new FileInputStream("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/messages.properties"));
 			properties_es.load(new FileInputStream("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/messages_es.properties"));
 			File f = new File("C:/charity/i18n.csv");
-			String headings = "Key,English,Spanish,French,Russian\n";
+			String headings = "Key\tEnglish\tSpanish\tFrench\tRussian\n";
 			FileOutputStream fos = new FileOutputStream(f);
 			fos.write(headings.getBytes());
 			SortedSet<String> keys = new TreeSet<String>(properties.stringPropertyNames());			
@@ -26,7 +26,7 @@ public class PropertiesToCSV {
 			{
 				String en_prop = properties.getProperty(s);
 				String es_prop = properties_es.getProperty(s);
-				String line = s + "," + en_prop + "," + (es_prop == null ? "" : es_prop) + "\n";
+				String line = "" + s + "\t" + en_prop + "\t" + (es_prop == null ? "" : es_prop) + "\n";
 				fos.write(line.getBytes());
 			}
 			fos.close();
