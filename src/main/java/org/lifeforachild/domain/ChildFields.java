1d7db7d44358a80a75c4c67c0198a42ef3d01134
serenapotts@gmail.com <serenapotts@users.noreply.github.com>
2011-10-16 11:08:37 +0000

    fix spelling errors

--- src/main/java/org/lifeforachild/domain/ChildFields.java
+++ src/main/java/org/lifeforachild/domain/ChildFields.java
@@ -2,14 +2,14 @@ package org.lifeforachild.domain;
 
 public enum ChildFields {
 	DATE_OF_BIRTH("Date of Birth"),	
-	INITIALS("Initals"),
+	INITIALS("Initials"),
 	ETHNIC_GROUP("Ethnic Group"),
-	SURVIVAL_STATUS("Survial Status"),
+	SURVIVAL_STATUS("Survival Status"),
 	DEATH("Death"),
 	DATE_REGO("Date of Registration"),
 	DIABETES_DIAGNOSED("Diabetes Diagnosed"),
 	INSULIN_SINCE("Insulin Since"),
-	AGE_AT_DIAGNOSIS("Calulated Age at Diagnosis"),
+	AGE_AT_DIAGNOSIS("Calculated Age at Diagnosis"),
 	SEX("Sex"),
 	DIABETES_TYPE("Diabetes Type"),
 	AGE_FIRST_INSULIN("Calculated Age at First Insulin");
