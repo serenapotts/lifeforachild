bf3172dd3eee9e00dbd680a8532ad5cd3416a9fa
serenapotts@gmail.com <serenapotts@users.noreply.github.com>
2013-02-03 02:01:04 +0000

    updates to language translations, mainly russian as given by alex

--- src/main/java/org/lifeforachild/Util/CSVtoProperties.java
+++ src/main/java/org/lifeforachild/Util/CSVtoProperties.java
@@ -10,9 +10,10 @@ import java.io.InputStreamReader;
 
 /*
  * Convert a file exported from excel as tab delimted text to properties file format.
- * The russian text is a special case. It needs to be exported separately.
+ * The russian text is a special case. It needs to be exported separately (as unicode text)
  * The open in notepad++, copy to eclipse and you will get the unescaped unicode characters.
  * Then paste this back to notepad++ in the properties file.
+ * Alternatively copy from notepad++ to message_ru.properties. run this and it will convert it to properties file format.
  */
 public class CSVtoProperties {
 
@@ -25,7 +26,7 @@ public class CSVtoProperties {
 			SortedProperties properties_fr = new SortedProperties();
 			SortedProperties properties_ru = new SortedProperties();
 			
-			File f = new File("C:/charity/i18n/20120618/i18n_reviewed_v6.txt");
+			File f = new File("C:/charity/i18n/translations4/i18n_reviewed_v8.txt");
 			FileInputStream fis = new FileInputStream(f);
 			DataInputStream in = new DataInputStream(fis);
 	        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
@@ -42,13 +43,29 @@ public class CSVtoProperties {
 			    	  properties_es.setProperty(propName, values[2]);
 			      if (values.length > 3)
 			    	  properties_fr.setProperty(propName, values[3]);
-			      if (values.length > 4)
-			    	  properties_ru.setProperty(propName, values[4]);
+			      //if (values.length > 4)
+			    //	  properties_ru.setProperty(propName, values[4]);
 		      }
 		    }
 		    properties.store(new FileOutputStream("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/new_messages.properties"), null);
 		    properties_es.store(new FileOutputStream("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/new_messages_es.properties"), null);
 		    properties_fr.store(new FileOutputStream("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/new_messages_fr.properties"), null);
+		    
+		    
+		    f = new File("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/messages_ru.properties");
+			fis = new FileInputStream(f);
+			in = new DataInputStream(fis);
+	        br = new BufferedReader(new InputStreamReader(fis));
+		    //Read File Line By Line
+		    while ((strLine = br.readLine()) != null)   {
+		      String[] values = strLine.split("\t");
+		      String propName = values[0];
+		      if (!propName.equals("Key"))
+		      {
+			      if (values.length > 1)
+			    	  properties_ru.setProperty(propName, values[1]);
+		      }
+		    }
 		    properties_ru.store(new FileOutputStream("C:/charity/lifeforachild/src/main/webapp/WEB-INF/i18n/new_messages_ru.properties"), null);
 		    //Close the input stream
 		    in.close();		
