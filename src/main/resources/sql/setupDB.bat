4ad6d02ed196482cb986ba91485e8f0f733f756b
serenapotts@gmail.com <serenapotts@users.noreply.github.com>
2014-03-26 10:49:29 +0000

    Fixes to get working on mac after updating to latest spring roo and STS

---
+++ src/main/resources/sql/setupDB.bat
@@ -0,0 +1,29 @@
+alias mysql=/usr/local/mysql/bin/mysql
+mysql -u root -e "create database if not exists life_for_a_child;"
+
+mysql -u root life_for_a_child -e "CREATE USER 'dev'@'localhost' IDENTIFIED BY 'password';"
+mysql -u root life_for_a_child -e "GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost' WITH GRANT OPTION;"
+
+mysql -u root life_for_a_child -e "source initialise/initialiseDB_schema.sql"
+mysql -u root life_for_a_child -e "source updates/001_users.sql"
+mysql -u root life_for_a_child -e "source updates/002_bmiage.sql"
+mysql -u root life_for_a_child -e "source updates/003_statage.sql"
+mysql -u root life_for_a_child -e "source updates/004_wtageinf.sql"
+mysql -u root life_for_a_child -e "source updates/005_wtage.sql"
+mysql -u root life_for_a_child -e "source updates/006_permissions.sql"
+mysql -u root life_for_a_child -e "source updates/007_20100923_update.sql"
+mysql -u root life_for_a_child -e "source updates/008_20110301_update.sql"
+mysql -u root life_for_a_child -e "source updates/009_mark_deleted_false.sql"
+mysql -u root life_for_a_child -e "source updates/010_sync_versions_tables.sql"
+mysql -u root life_for_a_child -e "source updates/011_new_child_fields.sql"
+mysql -u root life_for_a_child -e "source updates/012_new_report_fields.sql"
+mysql -u root life_for_a_child -e "source updates/triggers.sql"
+
+
+
+
+
+
+
+
+
