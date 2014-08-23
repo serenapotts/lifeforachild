af46f9381490c365f1127af55e3c4a67fbf68f6f
serenapotts <serenapotts@users.noreply.github.com>
2014-08-23 09:02:57 +0000

    enforce managers and admin have to create users with country and centre

--- src/main/java/org/lifeforachild/web/validation/UserValidator.java
+++ src/main/java/org/lifeforachild/web/validation/UserValidator.java
@@ -3,7 +3,10 @@ package org.lifeforachild.web.validation;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.lifeforachild.Util.SecurityUtil;
 import org.lifeforachild.domain.User;
+import org.lifeforachild.domain.UserGroup;
+import org.lifeforachild.enums.UserGroups;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -37,7 +40,19 @@ public class UserValidator implements Validator {
 				&& User.findUserbyUsername(user.getUsername()) != null)			
 		{
 			errors.rejectValue("username", "user.username.notUnique");
-		}		
+		}	
+		
+		UserGroup userGroup = SecurityUtil.getInstance().getCurrentUserGroup();
+		boolean isCountryCentreUser = (userGroup.getGroupCode().equals(UserGroups.COUNTRY_MANAGER.name()) || 
+				userGroup.getGroupCode().equals(UserGroups.TRANSLATION_COUNTRY_MANAGER.name()) || 
+				userGroup.getGroupCode().equals(UserGroups.CENTRE_ADMIN.name()));
+		if ( isCountryCentreUser&& user.getCountry() == null) {
+			errors.rejectValue("country", "user.country.required"); 
+		}
+		
+		if ( isCountryCentreUser &&  user.getCentre() == null) {
+			errors.rejectValue("centre", "user.centre.required"); 
+		}
 	}
 
 }
--- src/main/resources/sql/updates/017_usergroupcodes.sql
+++ src/main/resources/sql/updates/017_usergroupcodes.sql
@@ -2,3 +2,5 @@ update user_group set group_name ='usergroup.type.researchers' where group_name=
 update user_group set group_name ='usergroup.type.translation.country.manager' where group_name='Translation Country Manager';
 
 insert into message (message_key, version, english) values ('usergroup.type.translation.country.manager', 1, 'Translation Country Manager');
+insert into message (message_key, version, english) values ('user.country.required', 1, 'Country is required');
+insert into message (message_key, version, english) values ('user.centre.required', 1, 'Centre is required');
