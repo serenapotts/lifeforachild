9324b291b5fda4b1fccd65b5e5554a65a1a398c0
serenapotts <serenapotts@users.noreply.github.com>
2014-10-01 03:35:19 +0000

    fix typo

--- src/main/resources/sql/updates/018_errorMessages.sql
+++ src/main/resources/sql/updates/018_errorMessages.sql
@@ -1,2 +1,2 @@
 insert into message (message_key, version, english) values ('unexpectedError.titlepane', 1, 'Unexpected Error');
-insert into message (message_key, version, english) values ('unexpectedError.message', 1, 'An unexpetected error occured. Please contact Life For a Child by email via hte Help link below for assistance');
+insert into message (message_key, version, english) values ('unexpectedError.message', 1, 'An unexpetected error occured. Please contact Life For a Child by email via the Help link below for assistance');
