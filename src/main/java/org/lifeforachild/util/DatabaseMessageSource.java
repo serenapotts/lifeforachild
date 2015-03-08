e1d5a832b9070a89d6b06ab01cbce68d2094ecd2
Serena Keating <serenapotts@gmail.com>
2015-03-08 15:32:47 +1100

    Issue #11 - bug with translations that take arguments. removed old message files

--- src/main/java/org/lifeforachild/util/DatabaseMessageSource.java
+++ src/main/java/org/lifeforachild/util/DatabaseMessageSource.java
@@ -1,5 +1,6 @@
 package org.lifeforachild.util;
 
+import java.text.MessageFormat;
 import java.util.Locale;
 
 import org.lifeforachild.web.AppContext;
@@ -12,6 +13,15 @@ public class DatabaseMessageSource extends ReloadableResourceBundleMessageSource
 	@Override
 	protected String resolveCodeWithoutArguments(String code, Locale locale) {
 		return AppContext.getMessage(code, locale);
+	}
+	
+	@Override
+	protected MessageFormat resolveCode(String code, Locale locale) {
+		String message = AppContext.getMessage(code, locale);
+		if (message == null) {
+			return super.resolveCode(code, locale);
+		}
+		return new MessageFormat(message, locale);
 	}
 	
 	public void reload() {
