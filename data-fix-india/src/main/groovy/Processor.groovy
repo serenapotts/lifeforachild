5a342f520e7123e36fba0293386154a8edd54cd5
Linh Pham <linh.pham@rea-group.com>
2015-08-03 00:13:38 +1000

    Complete inserting to diabetes_centre, user, and user_versions

--- data-fix-india/src/main/groovy/Processor.groovy
+++ data-fix-india/src/main/groovy/Processor.groovy
@@ -7,6 +7,11 @@ class Processor {
     def opts
     def host
 
+    def sourceSql
+    def destSql
+
+    def STAGING_CENTRE_ID = 13
+
     Processor(opts) {
         log.level = Level.DEBUG
         // add an appender to log to file
@@ -20,38 +25,99 @@ class Processor {
         }
     }
 
-    String getInsertStatement(tableName, rowData) {
+    String getInsertStatementWithoutId(tableName, rowData) {
         assert !rowData.keySet().contains('id')
+        getInsertStatement(tableName, rowData)
+    }
+
+    String getInsertStatement(tableName, rowData) {
         def columns = rowData.keySet().join(',')
         def values = rowData.keySet().collect { rowData[it].getClass() == Boolean ? rowData[it] : "'${rowData[it]}'" }.join(',')
         "insert into $tableName ($columns) values ($values)"
     }
 
     void run() {
-        log.info "Host: $host"
-        log.info "User  : ${opts.u}"
-        log.info "Password: ${opts.p}"
-        log.info "Source schema: ${opts.s}"
-        log.info "Destination schema: ${opts.d}"
+        log.info "Host               : $host"
+        log.info "User               : ${opts.u}"
+        log.info "Password           : ${opts.p}"
+        log.info "Source schema      : ${opts.s}"
+        log.info "Destination schema : ${opts.d}"
 
         def sourceDb = [url: "jdbc:mysql://${host}:3306/${opts.s}", user: opts.u, password: opts.p, driver: 'com.mysql.jdbc.Driver']
-        def sourceSql = Sql.newInstance(sourceDb.url, sourceDb.user, sourceDb.password, sourceDb.driver)
+        sourceSql = Sql.newInstance(sourceDb.url, sourceDb.user, sourceDb.password, sourceDb.driver)
 
         def destDb = [url: "jdbc:mysql://${host}:3306/${opts.d}", user: opts.u, password: opts.p, driver: 'com.mysql.jdbc.Driver']
-        def destSql = Sql.newInstance(destDb.url, destDb.user, destDb.password, destDb.driver)
+        destSql = Sql.newInstance(destDb.url, destDb.user, destDb.password, destDb.driver)
 
+        def centreId = insertCentre()
+        def stagingToProdUserIdMap = insertUser(centreId)
+        insertUserVersions(centreId, stagingToProdUserIdMap)
+    }
+
+    Integer insertCentre() {
+        log.info "Inserting Centre..."
+        def centreId
         def diabetesCentreQuery = """
             select * from diabetes_centre
-            where id = 13
+            where id = $STAGING_CENTRE_ID
         """
         sourceSql.rows(diabetesCentreQuery).each { row ->
            row.remove("id")
-           def insert = getInsertStatement('diabetes_centre', row)
-
-           log.debug insert
-
+           def insert = getInsertStatementWithoutId('diabetes_centre', row)
            
+           log.debug "  Executing statement: $insert"
+           def result = destSql.executeInsert(insert)
+           centreId = result[0][0]
+           log.debug "  ID inserted: $centreId"
+        }
+        return centreId
+    }
+
+    Map insertUser(prodCentreId) {
+        log.info "Inserting User..."
+
+        def stagingToProdUserIdMap = [:]
+        def userQuery = """
+            select * from user
+            where centre = $STAGING_CENTRE_ID
+        """
+        sourceSql.rows(userQuery).each { row ->
+           def stagingUserId = row['id']
+
+           row.remove('id')
+           row['centre'] = prodCentreId   //update the centre ID
+           // user_group is the same for Staging and Prod database, so we don't need to modify reference key to user_group
+           def insert = getInsertStatementWithoutId('user', row)
            
+           log.debug "  Executing statement: $insert"
+           def result = destSql.executeInsert(insert)
+           def userId = result[0][0]
+           
+           stagingToProdUserIdMap[stagingUserId] = userId
+        }
+
+        log.debug("  stagingToProdUserIdMap = $stagingToProdUserIdMap")
+
+        return stagingToProdUserIdMap
+    }
+
+    void insertUserVersions(prodCentreId, stagingToProdUserIdMap) {
+        log.info "Inserting User Versions..."
+
+        def query = """
+            select * from user_versions
+            where centre = $STAGING_CENTRE_ID
+        """
+        sourceSql.rows(query).each { row ->
+           //update ID to point to the Production IDs
+           //update centre to point to Production centre ID
+           row['id'] = stagingToProdUserIdMap[row['id']]
+           row['centre'] = prodCentreId   //update the centre ID
+           // user_group is the same for Staging and Prod database, so we don't need to modify reference key to user_group
+           def insert = getInsertStatement('user_versions', row)
+           
+           log.debug "  Executing statement: $insert"
+           def result = destSql.executeInsert(insert)
         }
     }
 }
--- data-fix-india/src/test/groovy/ProcessorSpec.groovy
+++ data-fix-india/src/test/groovy/ProcessorSpec.groovy
@@ -1,13 +1,13 @@
 import spock.lang.*
 
 class ProcessorSpec extends Specification {
-    def "#getInsertStatement returns insert statement without id and correct columns and values"() {
+    def "#getInsertStatement returns insert statement with correct columns and values"() {
         given:
         def map = [address: 'No. 2, 1A Cross, Marenahalli, JP Nagar II Phase 560078 Bangalore, India', version: 0, is_deleted: false, name: 'Jnana Sanjeevini Medical Cente', country: 2]
 
         when:
         def processor = new Processor([])
-        def statement = processor.getInsertStatement('diabetes_centre', map)
+        def statement = processor.getInsertStatementWithoutId('diabetes_centre', map)
 
         then:
         statement == "insert into diabetes_centre (address,version,is_deleted,name,country) values ('No. 2, 1A Cross, Marenahalli, JP Nagar II Phase 560078 Bangalore, India','0',false,'Jnana Sanjeevini Medical Cente','2')"
